2a36f31da133c04369928d9a17eb5385dd467611
daiane.bellon <daianebellon@hotmail.com>
2022-01-20 11:42:36 -0300

    first commit

---
+++ src/test/java/br/com/daianebellon/profileservices/ProfileServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.daianebellon.profileservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
